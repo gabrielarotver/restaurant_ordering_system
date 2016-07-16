a382821b9c98a68c338288044768e3ae4597df2a
Gabriela Rotver <gabrielarotver86@gmail.com>
2016-07-16 16:06:14 -0400

    scaffolded items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716174833) do
+ActiveRecord::Schema.define(version: 20160716195039) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "first_name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20160716174833) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "orders", force: :cascade do |t|
